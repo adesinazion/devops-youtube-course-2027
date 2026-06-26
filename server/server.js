e9a6a70c4761fd2b2d5f4d3132593fc1a57cf8e7
zeedon <adesinazion@gmail.com>
2026-06-26 21:37:51 +0100

    dev: temp console changes

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('Changed again to - Shutting down gracefully init...');
   await mongoose.connection.close();
   process.exit(0);
 });
